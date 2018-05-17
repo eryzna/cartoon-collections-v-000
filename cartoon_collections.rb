284d645f9655559dee80584cb7b9d54cdcd19266
Elana Ryznar <eryzna@gmail.com>
2018-05-17 03:20:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include? do (cheese_types)
+  array.include? (cheese_types)
     puts "#{cheese_types}"
   end
 end
